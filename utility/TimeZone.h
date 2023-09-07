449d525e9fb68a8ade524bd50af206fc4be5ae96
wzhq199 <wzhq199@163.com>
2023-09-07 17:33:08 +0800

    add: TimeZone 实现 & TimeZone 单元测试 & Timestamp 单元测试

--- utility/TimeZone.h
+++ utility/TimeZone.h
@@ -8,48 +8,61 @@
 #include <time.h>
 
 namespace zhengqi {
-namespace utility {
-struct DateTime {
-  DateTime() {}
-  explicit DateTime(const struct tm &);
-  DateTime(int _year, int _month, int _day, int _hour, int _minute, int _second)
-      : year(_year), month(_month), day(_day), hour(_hour), minute(_minute),
-        second(_second) {}
-  string toIsoString() const;
+    namespace utility {
+        struct DateTime {
+            DateTime() {}
 
-  int year = 0;
-  int month = 0;
-  int day = 0;
-  int hour = 0;
-  int minute = 0;
-  int second = 0;
-};
+            explicit DateTime(const struct tm &);
 
-class TimeZone : copyable {
-public:
-  TimeZone() = default;
-  TimeZone(int eastOfUtc, const char *tzname);
+            DateTime(int _year, int _month, int _day, int _hour, int _minute, int _second)
+                    : year(_year), month(_month), day(_day), hour(_hour), minute(_minute),
+                      second(_second) {}
 
-  static TimeZone UTC();
-  static TimeZone China();
-  static TimeZone loadZoneFile(const char *zoneFile);
+            string toIsoString() const;
 
-  bool valid() const { return static_cast<bool>(data_); }
+            int year = 0;
+            int month = 0;
+            int day = 0;
+            int hour = 0;
+            int minute = 0;
+            int second = 0;
+        };
 
-  struct DateTime toLocalTime(int64_t secondsSinceEpoch,
-                              int *utcOffset = nullptr) const;
-  int64_t fromLocalTime(const struct DateTime &,
-                        bool postTransition = false) const;
+        class TimeZone : copyable {
+        public:
+            TimeZone() = default;
 
-  static struct DateTime toUtcTime(int64_t secondsSinceEpoch);
+            TimeZone(int eastOfUtc, const char *tzname);
 
-  struct Data;
+            static TimeZone UTC();
 
-private:
-  explicit TimeZone(std::unique_ptr<Data> data);
-  std::shared_ptr<Data> data_;
-  friend class TimeZoneTestPeer;
-};
-} // namespace utility
+            static TimeZone China();
+
+            static TimeZone loadZoneFile(const char *zoneFile);
+
+            bool valid() const { return static_cast<bool>(data_); }
+
+            struct DateTime toLocalTime(int64_t secondsSinceEpoch,
+                                        int *utcOffset = nullptr) const;
+
+            int64_t fromLocalTime(const struct DateTime &,
+                                  bool postTransition = false) const;
+
+            // gmtime(3)
+            static struct DateTime toUtcTime(int64_t secondsSinceEpoch);
+
+            // timegm(3)
+            static int64_t fromUtcTime(const struct DateTime &);
+
+            struct Data;
+
+        private:
+            explicit TimeZone(std::unique_ptr<Data> data);
+
+            std::shared_ptr<Data> data_;
+
+            friend class TimeZoneTestPeer;
+        };
+    } // namespace utility
 } // namespace zhengqi
 #endif // !DEBUG
