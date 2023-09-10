90670b653bbd0c0f12657db0afdccb7df908d570
wuzhengqi <709030194@qq.com>
2023-09-10 16:01:25 +0800

    add: SocketsOps 实现

---
+++ networking/Endian.h
@@ -0,0 +1,27 @@
+#ifndef ZHENGQI_CPP_NETWORKING_ENDIAN_H
+#define ZHENGQI_CPP_NETWORKING_ENDIAN_H
+
+#include <cstdint>
+#include <endian.h>
+#include <stdint.h>
+
+namespace zhengqi {
+namespace networking {
+namespace sockets {
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wconversion"
+#pragma GCC diagnostic ignored "-Wold-style-cast"
+inline uint64_t hostToNetwork64(uint64_t host64) { return htobe64(host64); }
+inline uint32_t hostToNetwork32(uint32_t host32) { return htobe32(host32); }
+inline uint16_t hostToNetwork16(uint16_t host16) { return htobe16(host16); }
+inline uint64_t networkToHost64(uint64_t net64) { return be64toh(net64); }
+inline uint32_t networkToHost32(uint32_t net32) { return be32toh(net32); }
+inline uint16_t networkToHost16(uint16_t net16) { return be16toh(net16); }
+
+#pragma GCC diagnostic pop
+
+} // namespace sockets
+} // namespace networking
+} // namespace zhengqi
+
+#endif // !DEBUG
