9f6c8da6e0016d8b46bf346753af5e00b8c2b1b6
wuzhengqi <709030194@qq.com>
2023-09-12 23:31:06 +0800

    add: EPollPoller 实现

--- networking/Buffer.h
+++ networking/Buffer.h
@@ -14,261 +14,261 @@
 #include <vector>
 
 namespace zhengqi {
-    namespace networking {
-        class Buffer : public utility::copyable {
-        public:
-            static const size_t kCheapPrepend = 8;
-            static const size_t kInitialSize = 1024;
+namespace networking {
+class Buffer : public utility::copyable {
+public:
+  static const size_t kCheapPrepend = 8;
+  static const size_t kInitialSize = 1024;
 
-            explicit Buffer(size_t initialSize = kInitialSize)
-                    : buffer_(kCheapPrepend + initialSize), readerIndex_(kCheapPrepend),
-                      writerIndex_(kCheapPrepend) {
-                assert(readableBytes() == 0);
-                assert(writableBytes() == initialSize);
-                assert(prependableBytes() == kCheapPrepend);
-            }
+  explicit Buffer(size_t initialSize = kInitialSize)
+      : buffer_(kCheapPrepend + initialSize), readerIndex_(kCheapPrepend),
+        writerIndex_(kCheapPrepend) {
+    assert(readableBytes() == 0);
+    assert(writableBytes() == initialSize);
+    assert(prependableBytes() == kCheapPrepend);
+  }
 
-            void swap(Buffer &rhs) {
-                buffer_.swap(rhs.buffer_);
-                std::swap(readerIndex_, rhs.readerIndex_);
-                std::swap(writerIndex_, rhs.writerIndex_);
-            }
+  void swap(Buffer &rhs) {
+    buffer_.swap(rhs.buffer_);
+    std::swap(readerIndex_, rhs.readerIndex_);
+    std::swap(writerIndex_, rhs.writerIndex_);
+  }
 
-            size_t readableBytes() const { return writerIndex_ - readerIndex_; }
+  size_t readableBytes() const { return writerIndex_ - readerIndex_; }
 
-            size_t writableBytes() const { return buffer_.size() - writerIndex_; }
+  size_t writableBytes() const { return buffer_.size() - writerIndex_; }
 
-            size_t prependableBytes() const { return readerIndex_; }
+  size_t prependableBytes() const { return readerIndex_; }
 
-            const char *peek() const { return begin() + readerIndex_; }
+  const char *peek() const { return begin() + readerIndex_; }
 
-            const char *findCRLF() const {
-                const char *crlf = std::search(peek(), beginWrite(), kCRLF, kCRLF + 2);
-                return crlf == beginWrite() ? nullptr : crlf;
-            }
+  const char *findCRLF() const {
+    const char *crlf = std::search(peek(), beginWrite(), kCRLF, kCRLF + 2);
+    return crlf == beginWrite() ? nullptr : crlf;
+  }
 
-            const char *findCRLF(const char *start) const {
-                assert(peek() <= start);
-                assert(start <= beginWrite());
-                const char *crlf = std::search(start, beginWrite(), kCRLF, kCRLF + 2);
-                return crlf == beginWrite() ? nullptr : crlf;
-            }
+  const char *findCRLF(const char *start) const {
+    assert(peek() <= start);
+    assert(start <= beginWrite());
+    const char *crlf = std::search(start, beginWrite(), kCRLF, kCRLF + 2);
+    return crlf == beginWrite() ? nullptr : crlf;
+  }
 
-            const char *findEOL() const {
-                const void *eol = memchr(peek(), '\n', readableBytes());
-                return static_cast<const char *>(eol);
-            }
+  const char *findEOL() const {
+    const void *eol = memchr(peek(), '\n', readableBytes());
+    return static_cast<const char *>(eol);
+  }
 
-            const char *findEOL(const char *start) const {
-                assert(peek() <= start);
-                assert(start <= beginWrite());
+  const char *findEOL(const char *start) const {
+    assert(peek() <= start);
+    assert(start <= beginWrite());
 
-                const void *eol =
-                        memchr(start, '\n', static_cast<size_t>(beginWrite() - start));
-                return static_cast<const char *>(eol);
-            }
+    const void *eol =
+        memchr(start, '\n', static_cast<size_t>(beginWrite() - start));
+    return static_cast<const char *>(eol);
+  }
 
-            void retrieve(size_t len) {
-                assert(len <= readableBytes());
-                if (len < readableBytes()) {
-                    readerIndex_ += len;
-                } else {
-                    retrieveAll();
-                }
-            }
+  void retrieve(size_t len) {
+    assert(len <= readableBytes());
+    if (len < readableBytes()) {
+      readerIndex_ += len;
+    } else {
+      retrieveAll();
+    }
+  }
 
-            void retrieveInt64() { retrieve(sizeof(int64_t)); }
+  void retrieveInt64() { retrieve(sizeof(int64_t)); }
 
-            void retrieveInt32() { retrieve(sizeof(int32_t)); }
+  void retrieveInt32() { retrieve(sizeof(int32_t)); }
 
-            void retrieveInt16() { retrieve(sizeof(int16_t)); }
+  void retrieveInt16() { retrieve(sizeof(int16_t)); }
 
-            void retrieveInt8() { retrieve(sizeof(int8_t)); }
+  void retrieveInt8() { retrieve(sizeof(int8_t)); }
 
-            void retrieveAll() {
-                readerIndex_ = kCheapPrepend;
-                writerIndex_ = kCheapPrepend;
-            }
+  void retrieveAll() {
+    readerIndex_ = kCheapPrepend;
+    writerIndex_ = kCheapPrepend;
+  }
 
-            std::string retrieveAllAsString() {
-                return retrieveAsString(readableBytes());
-            }
+  std::string retrieveAllAsString() {
+    return retrieveAsString(readableBytes());
+  }
 
-            std::string retrieveAsString(size_t len) {
-                assert(len <= readableBytes());
-                std::string result(peek(), len);
-                retrieve(len);
-                return result;
-            }
+  std::string retrieveAsString(size_t len) {
+    assert(len <= readableBytes());
+    std::string result(peek(), len);
+    retrieve(len);
+    return result;
+  }
 
-            utility::StringPiece toStringPiece() {
-                return utility::StringPiece(peek(), static_cast<int>(readableBytes()));
-            }
+  utility::StringPiece toStringPiece() {
+    return utility::StringPiece(peek(), static_cast<int>(readableBytes()));
+  }
 
-            void append(const utility::StringPiece &str) {
-                append(str.data(), str.size());
-            }
+  void append(const utility::StringPiece &str) {
+    append(str.data(), str.size());
+  }
 
-            void append(const char *data, size_t len) {
-                ensureWritableBytes(len);
-                std::copy(data, data + len, beginWrite());
-                hasWritten(len);
-            }
+  void append(const char *data, size_t len) {
+    ensureWritableBytes(len);
+    std::copy(data, data + len, beginWrite());
+    hasWritten(len);
+  }
 
-            void append(const void *data, size_t len) {
-                append(static_cast<const char *>(data), len);
-            }
+  void append(const void *data, size_t len) {
+    append(static_cast<const char *>(data), len);
+  }
 
-            void ensureWritableBytes(size_t len) {
-                if (writableBytes() < len) {
-                    makeSpace(len);
-                }
-                assert(writableBytes() >= len);
-            }
+  void ensureWritableBytes(size_t len) {
+    if (writableBytes() < len) {
+      makeSpace(len);
+    }
+    assert(writableBytes() >= len);
+  }
 
-            void hasWritten(size_t len) {
-                assert(len <= writableBytes());
-                writerIndex_ += len;
-            }
+  void hasWritten(size_t len) {
+    assert(len <= writableBytes());
+    writerIndex_ += len;
+  }
 
-            char *beginWrite() { return begin() + writerIndex_; }
+  char *beginWrite() { return begin() + writerIndex_; }
 
-            const char *beginWrite() const { return begin() + writerIndex_; }
+  const char *beginWrite() const { return begin() + writerIndex_; }
 
-            void unwrite(size_t len) {
-                assert(len <= readableBytes());
-                writerIndex_ -= len;
-            }
+  void unwrite(size_t len) {
+    assert(len <= readableBytes());
+    writerIndex_ -= len;
+  }
 
-            void appendInt64(int64_t x) {
-                int64_t be64 = sockets::hostToNetwork64(x);
-                append(&be64, sizeof be64);
-            }
+  void appendInt64(int64_t x) {
+    int64_t be64 = sockets::hostToNetwork64(x);
+    append(&be64, sizeof be64);
+  }
 
-            void appendInt32(int32_t x) {
-                int32_t be32 = sockets::hostToNetwork32(x);
-                append(&be32, sizeof be32);
-            }
+  void appendInt32(int32_t x) {
+    int32_t be32 = sockets::hostToNetwork32(x);
+    append(&be32, sizeof be32);
+  }
 
-            void appendInt16(int16_t x) {
-                int16_t be16 = sockets::hostToNetwork16(x);
-                append(&be16, sizeof be16);
-            }
+  void appendInt16(int16_t x) {
+    int16_t be16 = sockets::hostToNetwork16(x);
+    append(&be16, sizeof be16);
+  }
 
-            void appendInt8(int8_t x) { append(&x, sizeof x); }
+  void appendInt8(int8_t x) { append(&x, sizeof x); }
 
-            int64_t readInt64() {
-                int64_t result = peekInt64();
-                retrieveInt64();
-                return result;
-            }
+  int64_t readInt64() {
+    int64_t result = peekInt64();
+    retrieveInt64();
+    return result;
+  }
 
-            int32_t readInt32() {
-                int32_t result = peekInt32();
-                retrieveInt32();
-                return result;
-            }
+  int32_t readInt32() {
+    int32_t result = peekInt32();
+    retrieveInt32();
+    return result;
+  }
 
-            int16_t readInt16() {
-                int16_t result = peekInt16();
-                retrieveInt16();
-                return result;
-            }
+  int16_t readInt16() {
+    int16_t result = peekInt16();
+    retrieveInt16();
+    return result;
+  }
 
-            int8_t readInt8() {
-                int8_t result = peekInt8();
-                retrieveInt8();
-                return result;
-            }
+  int8_t readInt8() {
+    int8_t result = peekInt8();
+    retrieveInt8();
+    return result;
+  }
 
-            int64_t peekInt64() const {
-                assert(readableBytes() >= sizeof(int64_t));
-                int64_t be64 = 0;
-                ::memcpy(&be64, peek(), sizeof be64);
-                return sockets::networkToHost64(be64);
-            }
+  int64_t peekInt64() const {
+    assert(readableBytes() >= sizeof(int64_t));
+    int64_t be64 = 0;
+    ::memcpy(&be64, peek(), sizeof be64);
+    return sockets::networkToHost64(be64);
+  }
 
-            int32_t peekInt32() const {
-                assert(readableBytes() >= sizeof(int32_t));
-                int32_t be32 = 0;
-                ::memcpy(&be32, peek(), sizeof be32);
-                return sockets::networkToHost32(be32);
-            }
+  int32_t peekInt32() const {
+    assert(readableBytes() >= sizeof(int32_t));
+    int32_t be32 = 0;
+    ::memcpy(&be32, peek(), sizeof be32);
+    return sockets::networkToHost32(be32);
+  }
 
-            int16_t peekInt16() const {
-                assert(readableBytes() >= sizeof(int16_t));
-                int16_t be16 = 0;
-                ::memcpy(&be16, peek(), sizeof be16);
-                return sockets::networkToHost16(be16);
-            }
+  int16_t peekInt16() const {
+    assert(readableBytes() >= sizeof(int16_t));
+    int16_t be16 = 0;
+    ::memcpy(&be16, peek(), sizeof be16);
+    return sockets::networkToHost16(be16);
+  }
 
-            int8_t peekInt8() const {
-                assert(readableBytes() >= sizeof(int8_t));
-                int8_t be8 = *peek();
-                return be8;
-            }
+  int8_t peekInt8() const {
+    assert(readableBytes() >= sizeof(int8_t));
+    int8_t be8 = *peek();
+    return be8;
+  }
 
-            void prependInt64(int64_t x) {
-                int64_t be64 = sockets::hostToNetwork64(x);
-                prepend(&be64, sizeof be64);
-            }
+  void prependInt64(int64_t x) {
+    int64_t be64 = sockets::hostToNetwork64(x);
+    prepend(&be64, sizeof be64);
+  }
 
-            void prependInt32(int32_t x) {
-                int32_t be32 = sockets::hostToNetwork32(x);
-                prepend(&be32, sizeof be32);
-            }
+  void prependInt32(int32_t x) {
+    int32_t be32 = sockets::hostToNetwork32(x);
+    prepend(&be32, sizeof be32);
+  }
 
-            void prependInt16(int16_t x) {
-                int16_t be16 = sockets::hostToNetwork16(x);
-                prepend(&be16, sizeof be16);
-            }
+  void prependInt16(int16_t x) {
+    int16_t be16 = sockets::hostToNetwork16(x);
+    prepend(&be16, sizeof be16);
+  }
 
-            void prependInt8(int8_t x) { prepend(&x, sizeof x); }
+  void prependInt8(int8_t x) { prepend(&x, sizeof x); }
 
-            void prepend(const void *data, size_t len) {
-                assert(len <= prependableBytes());
-                readerIndex_ -= len;
-                const char *d = static_cast<const char *>(data);
-                std::copy(d, d + len, begin() + readerIndex_);
-            }
+  void prepend(const void *data, size_t len) {
+    assert(len <= prependableBytes());
+    readerIndex_ -= len;
+    const char *d = static_cast<const char *>(data);
+    std::copy(d, d + len, begin() + readerIndex_);
+  }
 
-            void shrink(size_t reserve) {
-                Buffer other;
-                other.ensureWritableBytes(readableBytes() + reserve);
-                other.append(toStringPiece());
-                swap(other);
-            }
+  void shrink(size_t reserve) {
+    Buffer other;
+    other.ensureWritableBytes(readableBytes() + reserve);
+    other.append(toStringPiece());
+    swap(other);
+  }
 
-            size_t internalCapacity() const { return buffer_.capacity(); }
+  size_t internalCapacity() const { return buffer_.capacity(); }
 
-            ssize_t readFd(int fd, int *savedErrno);
+  ssize_t readFd(int fd, int *savedErrno);
 
-        private:
-            char *begin() { return &*buffer_.begin(); }
+private:
+  char *begin() { return &*buffer_.begin(); }
 
-            const char *begin() const { return &*buffer_.begin(); }
+  const char *begin() const { return &*buffer_.begin(); }
 
-            void makeSpace(size_t len) {
-                if (writableBytes() + prependableBytes() < len + kCheapPrepend) {
-                    buffer_.resize(writerIndex_ + len);
-                } else {
-                    assert(kCheapPrepend < readerIndex_);
-                    size_t readable = readableBytes();
-                    std::copy(begin() + readerIndex_, begin() + writerIndex_,
-                              begin() + kCheapPrepend);
-                    readerIndex_ = kCheapPrepend;
-                    writerIndex_ = readerIndex_ + readable;
-                    assert(readable == readableBytes());
-                }
-            }
+  void makeSpace(size_t len) {
+    if (writableBytes() + prependableBytes() < len + kCheapPrepend) {
+      buffer_.resize(writerIndex_ + len);
+    } else {
+      assert(kCheapPrepend < readerIndex_);
+      size_t readable = readableBytes();
+      std::copy(begin() + readerIndex_, begin() + writerIndex_,
+                begin() + kCheapPrepend);
+      readerIndex_ = kCheapPrepend;
+      writerIndex_ = readerIndex_ + readable;
+      assert(readable == readableBytes());
+    }
+  }
 
-        private:
-            std::vector<char> buffer_;
-            size_t readerIndex_;
-            size_t writerIndex_;
+private:
+  std::vector<char> buffer_;
+  size_t readerIndex_;
+  size_t writerIndex_;
 
-            static const char kCRLF[];
-        };
-    } // namespace networking
+  static const char kCRLF[];
+};
+} // namespace networking
 } // namespace zhengqi
 #endif // !DEBUG
--- networking/poller/EPollPoller.cpp
+++ networking/poller/EPollPoller.cpp
@@ -6,6 +6,7 @@
 
 #include "networking/Channel.h"
 #include "utility/Logging.h"
+#include "utility/Types.h"
 
 #include <assert.h>
 #include <errno.h>
@@ -32,3 +33,81 @@ const int kDeleted = 2;
 EPollPoller::EPollPoller(EventLoop *loop)
     : Poller(loop), epollfd_(::epoll_create1(EPOLL_CLOEXEC)),
       events_(kInitEventListSize) {}
+
+EPollPoller::~EPollPoller() { ::close(epollfd_); }
+
+Timestamp EPollPoller::poll(int timeoutMs, ChannelList *activeChannels) {
+  LOG_TRACE << "fd total count " << channels_.size();
+  int numEvents = ::epoll_wait(epollfd_, &*events_.begin(),
+                               static_cast<int>(events_.size()), timeoutMs);
+
+  int savedErrno = errno;
+  Timestamp now(Timestamp::now());
+  if (numEvents > 0) {
+    LOG_TRACE << numEvents << " events happened";
+    fillActiveChannels(numEvents, activeChannels);
+    if (implicit_cast<size_t>(numEvents) == events_.size()) {
+      events_.resize(events_.size() * 2);
+    }
+  } else if (numEvents == 0) {
+    LOG_TRACE << "nothing happened";
+  } else {
+    if (savedErrno != EINTR) {
+      errno = savedErrno;
+      LOG_SYSERR << "EPollPoller::poll()";
+    }
+  }
+  return now;
+}
+
+void EPollPoller::fillActiveChannels(int numEvents,
+                                     ChannelList *activeChannels) const {
+  assert(implicit_cast<size_t>(numEvents) <= events_.size());
+  for (int i = 0; i < numEvents; i++) {
+    Channel *channel = static_cast<Channel *>(events_[i].data.ptr);
+#ifndef NDEBUG
+    int fd = channel->fd();
+    ChannelMap::const_iterator it = channels_.find(fd);
+    assert(it != channels_.end());
+    assert(it->second == channel);
+#endif // !DEBUG
+    channel->set_revents(events_[i].events);
+    activeChannels->push_back(channel);
+  }
+}
+
+void EPollPoller::updateChannel(Channel *channel) {
+  Poller::assertInLoopThread();
+  const int index = channel->index();
+  LOG_TRACE << "fd = " << channel->fd() << " events = " << channel->events()
+            << " index = " << index;
+
+  if (index == kNew || index == kDeleted) {
+    // a new one, add with EPOLL_CTL_ADD
+    int fd = channel->fd();
+    if (index == kNew) {
+      assert(channels_.find(fd) == channels_.end());
+      channels_[fd] = channel;
+    } else {
+      // index == kDeleted
+      assert(channels_.find(fd) != channels_.end());
+      assert(channels_[fd] == channel);
+    }
+
+    channel->set_index(kAdded);
+    update(EPOLL_CTL_ADD, channel);
+  } else {
+    // update existing one with EPOLL_CTL_MOD/DEL
+    int fd = channel->fd();
+    (void)fd;
+    assert(channels_.find(fd) != channels_.end());
+    assert(channels_[fd] == channel);
+    assert(index == kAdded);
+    if (channel->isNoneEvent()) {
+      update(EPOLL_CTL_DEL, channel);
+      channel->set_index(kDeleted);
+    } else {
+      update(EPOLL_CTL_MOD, channel);
+    }
+  }
+}
