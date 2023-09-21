23e19ddaa9e2cb34d7dd9cda1158c4cffaf47387
wuzhengqi <709030194@qq.com>
2023-09-21 20:31:03 +0800

    add: curl support

--- networking-examples/cdns/dns.cpp
+++ networking-examples/cdns/dns.cpp
@@ -24,4 +24,23 @@ void resolve(Resolver *res, const std::string &host) {
   res->resolve(host, std::bind(&resolveCallback, host, _1));
 }
 
-int main() { return 0; }
+int main(int argc, char *argv[]) {
+  EventLoop loop;
+  loop.runAfter(10, quit);
+  g_loop = &loop;
+  Resolver resolver(&loop, argc == 1 ? Resolver::kDNSonly
+                                     : Resolver::kDNSandHostsFile);
+  if (argc == 1) {
+    total = 3;
+    resolve(&resolver, "www.chenshuo.com");
+    resolve(&resolver, "www.example.com");
+    resolve(&resolver, "www.google.com");
+  } else {
+    total = argc - 1;
+    for (int i = 1; i < argc; i++) {
+      resolve(&resolver, argv[i]);
+    }
+  }
+  loop.loop();
+  return 0;
+}
